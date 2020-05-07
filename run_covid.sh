7398b8f7948243766e7fff57e8a1e4b58522c7c4
Richard Trotta <trotta@cua.edu>
2020-05-06 20:12:34 -0400

    Included weights with testing

--- run_covid.sh
+++ run_covid.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-05-06 01:32:38 trottar"
+# Time-stamp: "2020-05-06 19:49:30 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -13,12 +13,12 @@
 
 cd covid-19-data/scripts/scripts/
 echo "Updating COVID-19 dataset"
-python3 ecdc.py
+# python3 ecdc.py
 
 cd ../../../src/
 echo "Plotting dataset"
 python3 plot_covid.py
 
 cd ../OUTPUTS/
-convert us_cd.png cd_all.png cd_all_capita.png cd_all_wcapita.png cd_all_ratio.png covid_plots.pdf
+convert us_cd.png cd_all.png cd_all_capita.png cd_all_w_hc_capita.png cd_all_ratio.png cd_all_w_tests_capita.png cd_all_w_hc_tests_capita.png covid_plots.pdf
 rm -rf *.png
--- src/plot_covid.py
+++ src/plot_covid.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-05-06 18:43:50 trottar"
+# Time-stamp: "2020-05-06 20:09:27 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -26,15 +26,32 @@ c = klt.pyPlot(None)
 # inp_f = "../covid-19-data/public/data/ecdc/full_data.csv"
 inp_case = "../covid-19-data/public/data/ecdc/total_cases.csv"
 inp_death = "../covid-19-data/public/data/ecdc/total_deaths.csv"
-inp_test = "../covid-19-data/public/data/testing/total_deaths.csv"
+inp_test = "../covid-19-data/public/data/testing/covid-testing-latest-data-source-details.csv"
 
 try:
     case_data = dict(pd.read_csv(inp_case))
     death_data = dict(pd.read_csv(inp_death))
-    test_data = dict(pd.read_csv(inp_death))
+    test_data = dict(pd.read_csv(inp_test))
 except IOError:
     print("Error: %s or %s [plz]does not appear to exist." % (inp_death,case_data))
 
+# Testing 
+for i,evt in enumerate(test_data["ISO code"]):
+    if "USA" in evt:
+        us_test = test_data["Cumulative total"][i]
+    if "CHN" in evt:
+        china_test = test_data["Cumulative total"][i]
+    else:
+        china_test = 0
+    if "DE" in evt:
+        de_test = test_data["Cumulative total"][i]
+    if "KR" in evt:
+        sk_test = test_data["Cumulative total"][i]
+    if "IT" in evt:
+        itl_test = test_data["Cumulative total"][i]
+    if "GB" in evt:
+        uk_test = test_data["Cumulative total"][i]
+        
 # Cases and deaths
 us_c = np.array(case_data["United States"])
 us_d = np.array(death_data["United States"])
@@ -64,6 +81,16 @@ de_pop = 83.02e6
 sk_pop = 51.64e6
 itl_pop = 60.36e6
 uk_pop = 66.65e6
+
+
+# Testing per capita, really I am finding the number of the population not tested
+us_tpc = 1-(us_test/us_pop)
+china_tpc = 1-(china_test/china_pop) # no data
+de_tpc = 1-(de_test/de_pop)
+sk_tpc = 1-(sk_test/sk_pop)
+itl_tpc = 1-(itl_test/itl_pop)
+uk_tpc = 1-(uk_test/uk_pop)
+
 
 # Health care ranks normalized to 100 (source: https://www.who.int/healthinfo/paper30.pdf and https://worldpopulationreview.com/countries/best-healthcare-in-the-world/)
 us_hcr = 37/100
@@ -96,12 +123,12 @@ f.savefig('../OUTPUTS/us_cd.png')
 f = plt.figure(figsize=(11.69,8.27))
 plt.style.use('default')
 
-plt.scatter(us_c, us_d, label="US data #37")
-plt.scatter(china_c, china_d, label="China data #144")
-plt.scatter(de_c, de_d, label="Germany data #58")
-plt.scatter(sk_c, sk_d, label="South Korea data #2")
+plt.scatter(us_c, us_d, label="US data")
+plt.scatter(china_c, china_d, label="China data")
+plt.scatter(de_c, de_d, label="Germany data")
+plt.scatter(sk_c, sk_d, label="South Korea data")
 plt.scatter(itl_c, itl_d, label="Italy data")
-plt.scatter(uk_c, uk_d, label="UK data #18")
+plt.scatter(uk_c, uk_d, label="UK data")
 
 # The actual curve fitting happens here
 optimizedParameters, pcov = opt.curve_fit(fit, us_c, us_d,maxfev=10000)
@@ -146,27 +173,77 @@ f.savefig('../OUTPUTS/cd_all_capita.png')
 f = plt.figure(figsize=(11.69,8.27))
 plt.style.use('default')
 
-plt.scatter((us_c*us_hcr)/us_pop, (us_d*us_hcr)/us_pop, label="US data #37")
-plt.scatter((china_c*china_hcr)/china_pop, (china_d*china_hcr)/china_pop, label="China data #144")
-plt.scatter((de_c*de_hcr)/de_pop, (de_d*de_hcr)/de_pop, label="Germany data #25")
-plt.scatter((sk_c*sk_hcr)/sk_pop, (sk_d*sk_hcr)/sk_pop, label="South Korea data #58")
-plt.scatter((itl_c*itl_hcr)/itl_pop, (itl_d*itl_hcr)/itl_pop, label="Italy data #2")
-plt.scatter((uk_c*uk_hcr)/uk_pop, (uk_d*uk_hcr)/uk_pop, label="UK data #18")
+plt.scatter((us_c)/us_pop, (us_d*us_hcr)/us_pop, label="US data #37")
+plt.scatter((china_c)/china_pop, (china_d*china_hcr)/china_pop, label="China data #144")
+plt.scatter((de_c)/de_pop, (de_d*de_hcr)/de_pop, label="Germany data #25")
+plt.scatter((sk_c)/sk_pop, (sk_d*sk_hcr)/sk_pop, label="South Korea data #58")
+plt.scatter((itl_c)/itl_pop, (itl_d*itl_hcr)/itl_pop, label="Italy data #2")
+plt.scatter((uk_c)/uk_pop, (uk_d*uk_hcr)/uk_pop, label="UK data #18")
 
 # The actual curve fitting happens here
-optimizedParameters, pcov = opt.curve_fit(fit, (us_c*us_hcr)/us_pop, (us_d*us_hcr)/us_pop,maxfev=10000)
+optimizedParameters, pcov = opt.curve_fit(fit, (us_c)/us_pop, (us_d*us_hcr)/us_pop,maxfev=10000)
 
 # Use the optimized parameters to plot the best fit
-plt.plot((us_c*us_hcr)/us_pop, fit((us_c*us_hcr)/us_pop, *optimizedParameters), label="fit [$a*e^{-bx}+c$]");
+plt.plot((us_c)/us_pop, fit((us_c)/us_pop, *optimizedParameters), label="fit [$a*e^{-bx}+c$]");
 
 plt.legend(loc=0)
 plt.xscale('log')
 # plt.yscale('log')
-plt.xlabel('log(Cases per capita, weighted by health care rank)', fontsize =16)
+plt.xlabel('log(Cases per capita)', fontsize =16)
 plt.ylabel('Deaths per capita, weighted by health care rank', fontsize =16)
-plt.title('US Death vs Case (per capita, weighted by health care rank)', fontsize =20)
+plt.title('US Death vs Case (per capita, y-[weighted by health care rank])', fontsize =20)
 
-f.savefig('../OUTPUTS/cd_all_wcapita.png')
+f.savefig('../OUTPUTS/cd_all_w_hc_capita.png')
+
+f = plt.figure(figsize=(11.69,8.27))
+plt.style.use('default')
+
+plt.scatter((us_c)/us_pop, (us_d*us_tpc)/us_pop, label="US data")
+plt.scatter((china_c)/china_pop, (china_d*china_tpc)/china_pop, label="China, ***no data")
+plt.scatter((de_c)/de_pop, (de_d*de_tpc)/de_pop, label="Germany data")
+plt.scatter((sk_c)/sk_pop, (sk_d*sk_tpc)/sk_pop, label="South Korea data")
+plt.scatter((itl_c)/itl_pop, (itl_d*itl_tpc)/itl_pop, label="Italy data")
+plt.scatter((uk_c)/uk_pop, (uk_d*uk_tpc)/uk_pop, label="UK data")
+
+# The actual curve fitting happens here
+optimizedParameters, pcov = opt.curve_fit(fit, (us_c)/us_pop, (us_d*us_tpc)/us_pop,maxfev=10000)
+
+# Use the optimized parameters to plot the best fit
+plt.plot((us_c)/us_pop, fit((us_c)/us_pop, *optimizedParameters), label="fit [$a*e^{-bx}+c$]");
+
+plt.legend(loc=0)
+plt.xscale('log')
+# plt.yscale('log')
+plt.xlabel('log(Cases per capita)', fontsize =16)
+plt.ylabel('Deaths per capita, weighted by population untested', fontsize =16)
+plt.title('US Death vs Case (per capita, y-[weighted by population untested])', fontsize =20)
+
+f.savefig('../OUTPUTS/cd_all_w_tests_capita.png')
+
+f = plt.figure(figsize=(11.69,8.27))
+plt.style.use('default')
+
+plt.scatter((us_c)/us_pop, (us_d*us_tpc*us_hcr)/us_pop, label="US data")
+plt.scatter((china_c)/china_pop, (china_d*china_tpc*china_hcr)/china_pop, label="China, ***no data")
+plt.scatter((de_c)/de_pop, (de_d*de_tpc*de_hcr)/de_pop, label="Germany data")
+plt.scatter((sk_c)/sk_pop, (sk_d*sk_tpc*sk_hcr)/sk_pop, label="South Korea data")
+plt.scatter((itl_c)/itl_pop, (itl_d*itl_tpc*itl_hcr)/itl_pop, label="Italy data")
+plt.scatter((uk_c)/uk_pop, (uk_d*uk_tpc*uk_hcr)/uk_pop, label="UK data")
+
+# The actual curve fitting happens here
+optimizedParameters, pcov = opt.curve_fit(fit, (us_c)/us_pop, (us_d*us_tpc*us_hcr)/us_pop,maxfev=10000)
+
+# Use the optimized parameters to plot the best fit
+plt.plot((us_c)/us_pop, fit((us_c)/us_pop, *optimizedParameters), label="fit [$a*e^{-bx}+c$]");
+
+plt.legend(loc=0)
+plt.xscale('log')
+# plt.yscale('log')
+plt.xlabel('log(Cases per capita)', fontsize =16)
+plt.ylabel('Deaths per capita, weighted by untested  and hcr', fontsize =16)
+plt.title('US Death vs Case (per capita, y-[weighted by untested and hcr])', fontsize =20)
+
+f.savefig('../OUTPUTS/cd_all_w_hc_tests_capita.png')
 
 f = plt.figure(figsize=(11.69,8.27))
 plt.style.use('default')
